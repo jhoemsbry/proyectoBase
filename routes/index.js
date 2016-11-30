796b96ada6f833e7f49cd8473a93399caca1d203
jhoemsbry <jhemsbry@gmail.com>
2016-11-29 20:45:01 -0400

    cambios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var reclamo = {tipoReclamo:'odeca',observacion :'mal servicio',FechaReclamo:'29/11/2016'};
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Proyecto Final' ,subtitle: 'Avances en Computacion',reclamo:reclamo });
+
 });
 
 module.exports = router;
